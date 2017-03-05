4fd7a32fea2cbecea6cd8f4bd6f96af663d3c3a9
Jacob Young <jacob@jry.io>
2017-03-04 19:22:00 -0500

    Added react rendering with babel

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,2 +1,15 @@
-console.log('hello world')
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <p>React Rendered!</p>
+    )
+  }
+}
+
+console.log('testing')
+
+render(<App/>, document.getElementById('app'))
 
